4f601d40a46245aeab38b21c40910290d839353d
daffamra <daffamuhamadra@gmail.com>
2023-02-20 01:21:47 +0700

    tugas 1a sampai 3a

---
+++ tugas/tugas1/2a.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+        <?php 
+    $nrp = 13;
+
+    echo "aku adalah angka <b> $nrp </b>";
+    echo "<br>Jika aku dikali 5, maka aku sekarang akan menjadi ". "<b>". $nrp*5 . "</b>";
+    echo "<br>Jika aku dikali 5, maka aku sekarang akan menjadi ". "<b>". $nrp*5/2 . "</b>";
+    echo "<br>Jika aku dikali 5, maka aku sekarang akan menjadi ". "<b>". $nrp*5/2+75 . "</b>";
+    echo "<br>Jika aku dikali 5, maka aku sekarang akan menjadi ". "<b>". $nrp*5/2+75-20 . "</b>";
+    ?>
+</body>
+</html>
